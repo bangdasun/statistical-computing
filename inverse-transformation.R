d1598804971ea4e34d97b49754b926adb519fc0e
Bangda Sun <bangdasun94@gmail.com>
2017-05-19 12:21:50 -0400

    Update inverse-transformation.R

--- inverse-transformation.R
+++ inverse-transformation.R
@@ -38,11 +38,42 @@ Finverse = function(u) {
 
 u  = runif(10000, min = 0, max = 1)
 x  = Finverse(u)
-t  = seq(.001, 3, by = .001) 
+t  = seq(.001, 3.1, by = .1) 
 ft = forigin(t)
 
 library(ggplot2)
 ggplot() +
-  geom_histogram(aes(x, ..density..), binwidth = .05, fill = "lightblue", color = "black", 
+  geom_histogram(aes(x, ..density..), bins = 100, fill = "lightblue", color = "black", 
                  stat = "bin") +
-  geom_line(mapping = aes(x = t, y = ft), size = 1)
+  geom_line(mapping = aes(x = t, y = ft), size = 1) +
+  xlim(0, 3.1)
+
+
+### Example 2 ###
+# Suppose we want to sample from f(x) = 3*x^2, 0 < x < 1
+
+forigin = function(x) {
+  u = 3 * x^2
+  return(u)
+}
+
+Forigin = function(x) {
+  u = x^3
+  return(u)
+}
+
+Finverse = function(u) {
+  x = u^(1/3)
+  return(x)
+}
+
+u  = runif(10000)
+x  = Finverse(u)
+t  = seq(.01, 1.1, by = .1) 
+ft = forigin(t)
+
+ggplot() +
+  geom_histogram(aes(x, ..density..), binwidth = .1, fill = "lightblue", color = "black", 
+                 stat = "bin") +
+  geom_line(mapping = aes(x = t, y = ft), size = 1) +
+  xlim(0, 1)
