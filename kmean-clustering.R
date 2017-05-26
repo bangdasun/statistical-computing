ba3d88efcc1d252f09c99a7b41e879deb6c84dee
Bangda Sun <bangdasun94@gmail.com>
2017-05-26 12:07:03 -0400

    Update kmean-clustering.R

--- kmean-clustering.R
+++ kmean-clustering.R
@@ -34,3 +34,41 @@ dist = function (x, y) {
   d = sqrt((x[1] - x[2])^2 + (y[1] - y[2])^2)
   return(d)
 }
+
+# Initialization
+df$cluster2 = factor(sample(c(1,2), nrow(df), replace = TRUE))
+p1 = ggplot(df) + 
+  geom_point(mapping = aes(x = x, y = y, colour = cluster2), size = 2) + 
+  labs(title = "1")
+
+# Calculate center and distance
+center1_1 = c(mean(df$x[df$cluster2 == 1]), mean(df$y[df$cluster2 == 1]))
+center2_1 = c(mean(df$x[df$cluster2 == 2]), mean(df$y[df$cluster2 == 2]))
+p2 = ggplot() + 
+  geom_point(data = df, mapping = aes(x = x, y = y, colour = cluster2), size = 2) + 
+  labs(title = 2) +
+  geom_point(mapping = aes(x = center1_1[1], y = center1_1[2]), shape = 3, size = 3, color = "red") +
+  geom_point(mapping = aes(x = center2_1[1], y = center2_1[2]), shape = 3, size = 3, color = "blue")
+
+dist1 = apply(df[, c("x", "y")], MARGIN = 1, dist, center1_1)
+dist2 = apply(df[, c("x", "y")], MARGIN = 1, dist, center2_1)
+df$cluster3 = factor(ifelse(dist1 < dist2, 1, 2))
+
+p3 = ggplot() + 
+  geom_point(data = df, mapping = aes(x = x, y = y, colour = cluster3), size = 2) +
+  labs(title = 3) +
+  geom_point(mapping = aes(x = center1_1[1], y = center1_1[2]), shape = 3, size = 3, color = "red") +
+  geom_point(mapping = aes(x = center2_1[1], y = center2_1[2]), shape = 3, size = 3, color = "blue")
+
+center1_2 = c(mean(df$x[df$cluster3 == 1]), mean(df$y[df$cluster3 == 1]))
+center2_2 = c(mean(df$x[df$cluster3 == 2]), mean(df$y[df$cluster3 == 2]))
+
+p4 = ggplot() + 
+  geom_point(data = df, mapping = aes(x = x, y = y, colour = cluster3), size = 2) + 
+  labs(title = 4) +
+  geom_point(mapping = aes(x = center1_2[1], y = center1_2[2]), shape = 3, size = 3, color = "red") +
+  geom_point(mapping = aes(x = center2_2[1], y = center2_2[2]), shape = 3, size = 3, color = "blue")
+
+library(grid)
+library(gridExtra)
+grid.arrange(p1, p2, p3, p4, ncol = 2)
