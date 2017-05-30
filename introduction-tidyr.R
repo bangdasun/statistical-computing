6260ad3f791af54267d44d89a5353cabcc3bf905
Bangda Sun <bangdasun94@gmail.com>
2017-05-29 23:41:45 -0400

    Create introduction-tidyr.R

---
+++ introduction-tidyr.R
@@ -0,0 +1,45 @@
+# tidyr tutorial
+# Reference:
+#
+#   https://blog.rstudio.org/2014/07/22/introducing-tidyr/
+#
+
+# tidyr is a package to get tidy data, which means:
+#   (1) each column is a variable
+#   (2) each row is an observation
+
+# First: identify variables in the data, then move them into columns
+# tidyr mainly use:
+#   (1) gather(), like melt() in reshape2 package
+#   (2) separate()
+#   (3) spread()
+
+library(dplyr)
+library(tidyr)
+
+# Data: an experiment with 3 people, take 2 different drugs and record heart rate
+# original record form
+messydata = data.frame(
+  name = c("Wilbur", "Petunia", "Gregory"),
+  a    = c(67, 80, 64),
+  b    = c(56, 90, 50)
+)
+
+messydata
+messydata %>%
+  gather(drug, heartrate, a:b) # key - value pair
+
+# Data: how much time people spend on phones at home and work at two times
+messydata2 = data.frame(
+  id      = 1:4,
+  trt     = sample(rep(c("control", "treatment"), each = 2)),
+  work_T1 = runif(4),
+  home_T1 = runif(4),
+  work_T2 = runif(4),
+  home_T2 = runif(4)
+)
+
+messydata2
+messydata2 %>%
+  gather(key, time, -id, -trt) %>%                        # key - value
+  separate(key, into = c("location", "time"), sep = "_")  # separate key
